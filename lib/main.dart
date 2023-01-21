de0e9aa07a8efea9fee504819c8d67288f3bc544
Alexander <alexloparev@mail.ru>
2023-01-21 17:13:47 +0700

    Удалил лишние файлы

--- lib/main.dart
+++ lib/main.dart
@@ -1,41 +1,6 @@
 import 'package:flutter/material.dart';
 
-import '/pages/animated_map_controller.dart';
-import '/pages/circle.dart';
-import '/pages/custom_crs/custom_crs.dart';
-import '/pages/epsg3413_crs.dart';
-import '/pages/epsg4326_crs.dart';
-import '/pages/esri.dart';
-import '/pages/fallback_url_network_page.dart';
-import '/pages/fallback_url_offline_page.dart';
 import '/pages/home.dart';
-import '/pages/interactive_test_page.dart';
-import '/pages/latlng_to_screen_point.dart';
-import '/pages/live_location.dart';
-import '/pages/many_markers.dart';
-import '/pages/map_controller.dart';
-import '/pages/map_inside_listview.dart';
-import '/pages/marker_anchor.dart';
-import '/pages/marker_rotate.dart';
-import '/pages/max_bounds.dart';
-import '/pages/moving_markers.dart';
-import '/pages/network_tile_provider.dart';
-import '/pages/offline_map.dart';
-import '/pages/on_tap.dart';
-import '/pages/overlay_image.dart';
-import '/pages/plugin_scalebar.dart';
-import '/pages/plugin_zoombuttons.dart';
-import '/pages/point_to_latlng.dart';
-import '/pages/polygon.dart';
-import '/pages/polyline.dart';
-import '/pages/reset_tile_layer.dart';
-import '/pages/sliding_map.dart';
-import '/pages/stateful_markers.dart';
-import '/pages/tap_to_add.dart';
-import '/pages/tile_builder_example.dart';
-import '/pages/tile_loading_error_handle.dart';
-import '/pages/widgets.dart';
-import '/pages/wms_tile_layer.dart';
 
 void main() => runApp(const MyApp());
 
@@ -51,49 +16,6 @@ class MyApp extends StatelessWidget {
         primarySwatch: mapBoxBlue,
       ),
       home: const HomePage(),
-      routes: <String, WidgetBuilder>{
-        NetworkTileProviderPage.route: (context) =>
-            const NetworkTileProviderPage(),
-        WidgetsPage.route: (context) => const WidgetsPage(),
-        TapToAddPage.route: (context) => const TapToAddPage(),
-        EsriPage.route: (context) => const EsriPage(),
-        PolylinePage.route: (context) => const PolylinePage(),
-        MapControllerPage.route: (context) => const MapControllerPage(),
-        AnimatedMapControllerPage.route: (context) =>
-            const AnimatedMapControllerPage(),
-        MarkerAnchorPage.route: (context) => const MarkerAnchorPage(),
-        PluginScaleBar.route: (context) => const PluginScaleBar(),
-        PluginZoomButtons.route: (context) => const PluginZoomButtons(),
-        OfflineMapPage.route: (context) => const OfflineMapPage(),
-        OnTapPage.route: (context) => const OnTapPage(),
-        MarkerRotatePage.route: (context) => const MarkerRotatePage(),
-        MovingMarkersPage.route: (context) => const MovingMarkersPage(),
-        CirclePage.route: (context) => const CirclePage(),
-        OverlayImagePage.route: (context) => const OverlayImagePage(),
-        PolygonPage.route: (context) => const PolygonPage(),
-        SlidingMapPage.route: (_) => const SlidingMapPage(),
-        WMSLayerPage.route: (context) => const WMSLayerPage(),
-        CustomCrsPage.route: (context) => const CustomCrsPage(),
-        LiveLocationPage.route: (context) => const LiveLocationPage(),
-        TileLoadingErrorHandle.route: (context) =>
-            const TileLoadingErrorHandle(),
-        TileBuilderPage.route: (context) => const TileBuilderPage(),
-        InteractiveTestPage.route: (context) => const InteractiveTestPage(),
-        ManyMarkersPage.route: (context) => const ManyMarkersPage(),
-        StatefulMarkersPage.route: (context) => const StatefulMarkersPage(),
-        MapInsideListViewPage.route: (context) => const MapInsideListViewPage(),
-        ResetTileLayerPage.route: (context) => const ResetTileLayerPage(),
-        EPSG4326Page.route: (context) => const EPSG4326Page(),
-        EPSG3413Page.route: (context) => const EPSG3413Page(),
-        MaxBoundsPage.route: (context) => const MaxBoundsPage(),
-        PointToLatLngPage.route: (context) => const PointToLatLngPage(),
-        LatLngScreenPointTestPage.route: (context) =>
-            const LatLngScreenPointTestPage(),
-        FallbackUrlNetworkPage.route: (context) =>
-            const FallbackUrlNetworkPage(),
-        FallbackUrlOfflinePage.route: (context) =>
-            const FallbackUrlOfflinePage(),
-      },
     );
   }
 }
--- lib/widgets/drawer.dart
+++ lib/widgets/drawer.dart
@@ -1,41 +1,6 @@
 import 'package:flutter/material.dart';
 
-import '/pages/animated_map_controller.dart';
-import '/pages/circle.dart';
-import '/pages/custom_crs/custom_crs.dart';
-import '/pages/epsg3413_crs.dart';
-import '/pages/epsg4326_crs.dart';
-import '/pages/esri.dart';
-import '/pages/fallback_url_network_page.dart';
-import '/pages/fallback_url_offline_page.dart';
 import '/pages/home.dart';
-import '/pages/interactive_test_page.dart';
-import '/pages/latlng_to_screen_point.dart';
-import '/pages/live_location.dart';
-import '/pages/many_markers.dart';
-import '/pages/map_controller.dart';
-import '/pages/map_inside_listview.dart';
-import '/pages/marker_anchor.dart';
-import '/pages/marker_rotate.dart';
-import '/pages/max_bounds.dart';
-import '/pages/moving_markers.dart';
-import '/pages/network_tile_provider.dart';
-import '/pages/offline_map.dart';
-import '/pages/on_tap.dart';
-import '/pages/overlay_image.dart';
-import '/pages/plugin_scalebar.dart';
-import '/pages/plugin_zoombuttons.dart';
-import '/pages/point_to_latlng.dart';
-import '/pages/polygon.dart';
-import '/pages/polyline.dart';
-import '/pages/reset_tile_layer.dart';
-import '/pages/sliding_map.dart';
-import '/pages/stateful_markers.dart';
-import '/pages/tap_to_add.dart';
-import '/pages/tile_builder_example.dart';
-import '/pages/tile_loading_error_handle.dart';
-import '/pages/widgets.dart';
-import '/pages/wms_tile_layer.dart';
 
 Widget _buildMenuItem(
     BuildContext context, Widget title, String routeName, String currentRoute) {
